d81115198124a0516eb482921fec4ab28c71a8ac
Udit Jain <jainudit.012@gmail.com>
2024-05-26 23:30:37 +0530

    add fields

--- backend/models/user.js
+++ backend/models/user.js
@@ -6,6 +6,19 @@ const User = sequelize.define('User',
         user_id: {
             type: DataTypes.STRING,
             primaryKey: true
+        },
+        username: {
+            type: DataTypes.STRING,
+            unique: true,
+            allowNull: false
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        theme: {
+            type: DataTypes.STRING,
+            allowNull: true
         }
     }, 
     {
