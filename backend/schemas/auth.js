737faf1643699996d05b55832a12cf512475e188
Udit Jain <jainudit.012@gmail.com>
2024-05-27 02:13:47 +0530

    add theme

--- backend/schemas/auth.js
+++ backend/schemas/auth.js
@@ -2,7 +2,8 @@ const Joi = require('joi')
 
 const registerSchema = Joi.object({
     username: Joi.string().min(3).max(30).required(),
-    password: Joi.string().min(8).required()
+    password: Joi.string().min(8).required(),
+    theme: Joi.string()
 })
 
 const loginSchema = Joi.object({
