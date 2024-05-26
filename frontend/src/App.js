c29cd518a79d27c69d12456c234e7fe6a2e56fe4
Udit Jain <jainudit.012@gmail.com>
2024-05-27 02:22:07 +0530

    remove layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,6 @@ import 'react-toastify/dist/ReactToastify.css'
 import { ToastContainer } from 'react-toastify' 
 import { ThemeProvider } from './context/themeContext'
 import { AuthProvider } from './context/authContext'
-import Layout from './layout/layout'
 import Home from './pages/home'
 import LoginPage from './pages/login'
 import SignupPage from './pages/signup'
@@ -17,7 +16,6 @@ const App = () => {
         <ThemeProvider>
         <AuthProvider>
             <Routes>
-                <Route path="/" element={<Layout />} />
                 <Route index element={<Home />} />
                 <Route path="/login" element={<LoginPage />} />
                 <Route path="/signup" element={<SignupPage/>} />
