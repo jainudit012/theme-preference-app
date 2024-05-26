1b598593eb9eeaa21f5c11956fafcf5b7d5e5094
Udit Jain <jainudit.012@gmail.com>
2024-05-26 23:29:46 +0530

    add jwt secret and constant

---
+++ backend/startup/constants.js
@@ -0,0 +1,7 @@
+const DEFAULT_THEME = process.env.DEFAULT_THEME || 'light'
+const THEME_CHOICES = ['light', 'dark', 'blue', 'red']
+
+module.exports = {
+    DEFAULT_THEME,
+    THEME_CHOICES
+}
---
+++ backend/startup/secret.js
@@ -0,0 +1,5 @@
+const jwtSecret = process.env.JWT_SECRET || 'jwt_secret'
+
+module.exports = {
+    jwtSecret
+}
